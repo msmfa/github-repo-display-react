cf5cc9f3bc3a75de1fb6fa8d712eef9f4a6d357e
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-05-13 19:18:39 +0100

    Updates

---
+++ precache-manifest.eeb95a617e6efef926254d8e06a7259c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c55dac1907bb331398de2b7ea20b52",
+    "url": "/github-repo-display-react/index.html"
+  },
+  {
+    "revision": "c0b65772718af4434b97",
+    "url": "/github-repo-display-react/static/css/main.1e1dc1d3.chunk.css"
+  },
+  {
+    "revision": "66b5e923fb685d436042",
+    "url": "/github-repo-display-react/static/js/2.d3fffae6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-repo-display-react/static/js/2.d3fffae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b65772718af4434b97",
+    "url": "/github-repo-display-react/static/js/main.52929c74.chunk.js"
+  },
+  {
+    "revision": "2cd1e8fb3838be2fe06e",
+    "url": "/github-repo-display-react/static/js/runtime-main.8d84956b.js"
+  }
+]);
